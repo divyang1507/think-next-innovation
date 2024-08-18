7ea078b4a5fe499909ffd3260cc27af28bd0231e
divyang1507 <divhp1507@gmail.com>
2024-08-18 08:19:12 +0530

    framer gsap locomotive and split type commit

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,11 @@ import About from "./pages/About/About";
 import Service from "./pages/Service/Service";
 import Contact from "./pages/Contact/Contact";
 
+// import LocomotiveScroll from 'locomotive-scroll';
+
+
 export default function Home() {
+  // const scroll = new LocomotiveScroll();
   return (
     <main className={styles.main}>
       <Hero />
--- src/app/pages/Home/Hero.jsx
+++ src/app/pages/Home/Hero.jsx
@@ -1,6 +1,8 @@
+'use client'
 import Image from "next/image";
 import React from "react";
 import "./hero.css";
+import { motion } from "framer-motion"
 
 const Hero = () => {
   return (
@@ -8,8 +10,12 @@ const Hero = () => {
       <section className="hero">
         <div className="heroContainer">
           <div className="hero-1">
-            <h2 className="tag">Think IT, Think Next</h2>
-            <h1 className="maintag">Brand, Design & Development Agency</h1>
+           <div className="tag">
+           <motion.h2 initial={{y:-100}} animate={{y:"0%"}} transition={{ duration:1}} >Think IT, Think Next</motion.h2>
+           </div> 
+           <div className="maintag">
+            <motion.h1 initial={{y:-100}} animate={{y:"0%"}} transition={{ duration:1}} >Brand, Design & Development Agency</motion.h1>
+           </div>
             <p>
               At Think Next Innovation, Transforming ideas into innovative
               solutions. Partner with us to bring your visions to life and stay
